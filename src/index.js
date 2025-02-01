d492c42f3c6b1420ebeec94ab6819424e9153caa
gdamss <elbryan@hotmail.fr>
2025-02-01 09:08:05 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+//  string
+const name = "jean";
+
+// nombre
+const age = 18.5;
+
+// null
+const hobbies = null;
+
+// undefined
+let action;
+
+// symbole
+let sym = Symbol()
+
+// boolean
+let actif = true;
+
+// objet literral
+const ville = {
+    name: "Paris",
+    state: "France"
+};
+
+const greetings = function () {
+    console.log("hello");
+}
+
+
+const today = new Date();
+
+const arr = [1, 2, 3];
+console.log(typeof today );
